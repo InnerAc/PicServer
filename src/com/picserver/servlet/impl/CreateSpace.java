c51adfabcb84a294ecf729f1b54d31d7fc367378
sunzequn <sunzequn@live.cn>
2014-12-04 20:19:04 +0800

    修改servlet名

---
+++ src/com/picserver/servlet/impl/CreateSpace.java
@@ -0,0 +1,27 @@
+package com.picserver.servlet.impl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateSpace
+ */
+@WebServlet("/CreateSpace")
+public class CreateSpace extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public CreateSpace() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
