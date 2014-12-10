5269fc3c80ea4741c44db9c69a50e9e1fe5cd983
GMpj <good_mpj@163.com>
2014-12-10 21:40:42 +0800

    更改同步用户信息bug

--- src/com/picserver/hbase/ValueTable.java
+++ src/com/picserver/hbase/ValueTable.java
@@ -9,10 +9,14 @@ public static void main(String args[]){
 	UserBean user=new UserBean();
 	user.setUid("你好");
 	user.setPwd("123");
+	user.setPicNum("3");
+	user.setSpaceNum("3");
+	user.setTotSize("3");
 	writer.putUserBean(user);
 	SpaceBean space=new SpaceBean();
 	space.setKey("哈哈哈"+"你好");
 	space.setName("哈哈哈");
+	
 	writer.putSpaceBean(space);
 	
 }
